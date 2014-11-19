de1098be73699e9131a0024fd508977a72c78d55
Kyle Fuller <inbox@kylefuller.co.uk>
2014-11-19 22:54:18 +0100

    Create an Xcode project

--- PathKit.podspec
+++ PathKit.podspec
@@ -7,6 +7,7 @@ Pod::Spec.new do |spec|
   spec.author = { 'Kyle Fuller' => 'inbox@kylefuller.co.uk' }
   spec.social_media_url = 'http://twitter.com/kylefuller'
   spec.source = { :git => 'https://github.com/kylef/PathKit.git', :tag => "#{spec.version}" }
-  spec.source_files = 'PathKit.swift'
+  spec.source_files = 'PathKit/PathKit.swift'
+  spec.requires_arc = true
 end
 
---
+++ PathKit/PathKit.h
@@ -0,0 +1,19 @@
+//
+//  PathKit.h
+//  PathKit
+//
+//  Created by Kyle Fuller on 20/11/2014.
+//  Copyright (c) 2014 Cocode. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PathKit.
+FOUNDATION_EXPORT double PathKitVersionNumber;
+
+//! Project version string for PathKit.
+FOUNDATION_EXPORT const unsigned char PathKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PathKit/PublicHeader.h>
+
+
