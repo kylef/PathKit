93f7e12c9b4d2137a023e054f9542c0e8cbabb3e
Marius Rackwitz <git@mariusrackwitz.de>
2015-08-13 11:42:58 +0200

    [Doc] Document everything properly

--- PathKit/PathKit.swift
+++ PathKit/PathKit.swift
@@ -4,9 +4,11 @@ import Foundation
 
 /// Represents a filesystem path.
 public struct Path {
+    /// The character used by the OS to separate two path elements
     public static let separator = "/"
 
-    private var path: String
+    /// The underlying string representation
+    internal var path: String
 
     internal static var fileManager = NSFileManager.defaultManager()
 
@@ -69,19 +71,26 @@ extension Path : Hashable {
 // MARK: Path Info
 
 extension Path {
-    /** Method for testing whether a path is absolute.
-    :return: true if the path begings with a slash
-    */
+    /// Test whether a path is absolute.
+    ///
+    /// - Returns: `true` iff the path begings with a slash
+    ///
     public var isAbsolute: Bool {
         return path.hasPrefix(Path.separator)
     }
 
-    /// Returns true if a path is relative (not absolute)
+    /// Test whether a path is relative.
+    ///
+    /// - Returns: `true` iff a path is relative (not absolute)
+    ///
     public var isRelative: Bool {
         return !isAbsolute
     }
 
-    /// Returns the absolute path in the actual filesystem
+    /// Concatenates relative paths to the current directory and derives the normalized path
+    ///
+    /// - Returns: the absolute path in the actual filesystem
+    ///
     public func absolute() -> Path {
         if isAbsolute {
             return normalize()
@@ -92,6 +101,10 @@ extension Path {
 
     /// Normalizes the path, this cleans up redundant ".." and ".", double slashes
     /// and resolves "~".
+    ///
+    /// - Returns: a new path made by removing extraneous path components from the underlying String
+    ///   representation.
+    ///
     public func normalize() -> Path {
         return Path((self.path as NSString).stringByStandardizingPath)
     }
@@ -101,14 +114,21 @@ extension Path {
 // MARK: File Info
 
 extension Path {
-    /// Returns whether a file or directory exists at a specified path
+    /// Test whether a file or directory exists at a specified path
+    ///
+    /// - Returns: `false` iff the path doesn't exist on disk or its existence could not be
+    ///   determined
+    ///
     public var exists: Bool {
         return Path.fileManager.fileExistsAtPath(self.path)
     }
-    
-    /** Method for testing whether a path is a directory.
-    :return: true if the path exists on disk and is a directory
-    */
+
+    /// Test whether a path is a directory.
+    ///
+    /// - Returns: `true` if the path is a directory or a symbolic link that points to a directory;
+    ///   `false` if the path is not a directory or the path doesn't exist on disk or its existence
+    ///   could not be determined
+    ///
     public var isDirectory: Bool {
         var directory = ObjCBool(false)
         return Path.fileManager.fileExistsAtPath(normalize().path, isDirectory: &directory) && directory.boolValue
@@ -119,10 +139,20 @@ extension Path {
 // MARK: File Manipulation
 
 extension Path {
+    /// Delete the file or directory.
+    ///
+    /// - Note: If the path specifies a directory, the contents of that directory are recursively
+    ///   removed.
+    ///
     public func delete() throws -> () {
         try Path.fileManager.removeItemAtPath(self.path)
     }
 
+    /// Move the file or directory to a new location synchronously.
+    ///
+    /// - Parameter destination: The new path. This path must include the name of the file or
+    ///   directory in its new location.
+    ///
     public func move(destination: Path) throws -> () {
         try Path.fileManager.moveItemAtPath(self.path, toPath: destination.path)
     }
@@ -132,7 +162,10 @@ extension Path {
 // MARK: Current Directory
 
 extension Path {
-    // Returns the current working directory of the process
+    /// The current working directory of the process
+    ///
+    /// - Returns: the current working directory of the process
+    ///
     public static var current: Path {
         get {
             return self.init(Path.fileManager.currentDirectoryPath)
@@ -142,10 +175,13 @@ extension Path {
         }
     }
     
-    /** Changes the current working directory of the process to the path during the execution of the given block.
-    - parameter closure: A closure to be executed while the current directory is configured to the path.
-    :note: The original working directory is restored when the block exits.
-    */
+    /// Changes the current working directory of the process to the path during the execution of the
+    /// given block.
+    ///
+    /// - Note: The original working directory is restored when the block exits.
+    /// - Parameter closure: A closure to be executed while the current directory is configured to
+    ///   the path.
+    ///
     public func chdir(closure: (() -> ())) {
         let previous = Path.current
         Path.current = self
@@ -158,10 +194,21 @@ extension Path {
 // MARK: Contents
 
 extension Path {
+    /// Reads the file.
+    ///
+    /// - Returns: the contents of the file at the specified path.
+    ///
     public func read() -> NSData? {
         return Path.fileManager.contentsAtPath(self.path)
     }
 
+    /// Reads the file contents and encoded its bytes to string applying the given encoding.
+    ///
+    /// - Parameter encoding: the encoding which should be used to decode the data.
+    ///   (by default: `NSUTF8StringEncoding`)
+    ///
+    /// - Returns: the contents of the file at the specified path as string.
+    ///
     public func read(encoding: NSStringEncoding = NSUTF8StringEncoding) -> String? {
         if let data: NSData = read() {
             return NSString(data: data, encoding: encoding) as? String
@@ -170,10 +217,29 @@ extension Path {
         return nil
     }
 
+    /// Write a file.
+    ///
+    /// - Note: Works atomically: the data is written to a backup file, and then — assuming no
+    ///   errors occur — the backup file is renamed to the name specified by path.
+    ///
+    /// - Parameter data: the contents to write to file.
+    ///
     public func write(data: NSData) -> Bool {
         return data.writeToFile(normalize().path, atomically: true)
     }
 
+    /// Reads the file.
+    ///
+    /// - Note: Works atomically: the data is written to a backup file, and then — assuming no
+    ///   errors occur — the backup file is renamed to the name specified by path.
+    ///
+    /// - Parameter string: the string to write to file.
+    ///
+    /// - Parameter encoding: the encoding which should be used to represent the string as bytes.
+    ///   (by default: `NSUTF8StringEncoding`)
+    ///
+    /// - Returns: the contents of the file at the specified path as string.
+    ///
     public func write(string: String, encoding: NSStringEncoding = NSUTF8StringEncoding) -> Bool {
         if let data = string.dataUsingEncoding(encoding, allowLossyConversion: true) {
             return write(data)
@@ -207,9 +273,11 @@ extension Path {
 
 extension Path : Equatable {}
 
-/** Determines if two paths are identical
-:note: The comparison is string-based. Be aware that two different paths (foo.txt and ./foo.txt) can refer to the same file.
-*/
+/// Determines if two paths are identical
+///
+/// - Note: The comparison is string-based. Be aware that two different paths (foo.txt and
+///   ./foo.txt) can refer to the same file.
+///
 public func ==(lhs: Path, rhs: Path) -> Bool {
     return lhs.path == rhs.path
 }
