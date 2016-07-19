b4fe0ec86606842118411d6b8541cd27c558ae52
Kyle Fuller <kyle@fuller.li>
2016-07-19 19:47:25 +0100

    Use swim/Spectre for testing

--- Package.swift
+++ Package.swift
@@ -1,5 +1,8 @@
 import PackageDescription
 
 let package = Package(
-  name: "PathKit"
+  name: "PathKit",
+  dependencies: [
+    .Package(url: "https://github.com/kylef/Spectre", majorVersion: 0, minor: 7),
+  ]
 )
--- Pods/Target Support Files/CatchingFire/CatchingFire-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_CatchingFire : NSObject
-@end
-@implementation PodsDummy_CatchingFire
-@end
