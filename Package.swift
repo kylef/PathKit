26296824ac23bf65215a5214f6fee930fc5cb242
Valentin Knabel <dev@vknabel.com>
2016-12-16 17:54:27 +0100

    Use `.git` extension on dependencies URL

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
   name: "PathKit",
   dependencies: [
     // https://github.com/apple/swift-package-manager/pull/597
-    .Package(url: "https://github.com/kylef/Spectre", majorVersion: 0, minor: 7),
+    .Package(url: "https://github.com/kylef/Spectre.git", majorVersion: 0, minor: 7),
   ]
 )
