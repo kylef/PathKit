f76f0050a7005d84f66e1c1861f6d99edb5ec0d4
Jacob Williams <ponyboy47@gmail.com>
2017-06-01 22:23:27 -0600

    Moved String+ to a separate Strings package and made it a dependency of PathKit

--- Package.swift
+++ Package.swift
@@ -2,10 +2,8 @@ import PackageDescription
 
 let package = Package(
   name: "PathKit",
-  // Uncomment the following line once the public API for testDependencies is reimplemented
-  //testDependencies: [
   dependencies: [
-      .Package(url: "https://github.com/kylef/Spectre.git", majorVersion: 0, minor: 7)
+      .Package(url: "https://github.com/Ponyboy47/Strings.git", majorVersion: 1)
   ],
   exclude: [
       "Tests"
