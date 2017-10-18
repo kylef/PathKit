a667cd3ce03607420b0ec89b5e26f63766214e62
Keith Smiley <k@keith.so>
2017-10-18 13:20:03 -0700

    Update Package Description to 4.0 and Spectre version to 0.8.0 (#39)

--- Package.swift
+++ Package.swift
@@ -1,9 +1,17 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
   name: "PathKit",
+  products: [
+    .library(name: "PathKit", targets: ["PathKit"]),
+  ],
   dependencies: [
-    // https://github.com/apple/swift-package-manager/pull/597
-    .Package(url: "https://github.com/kylef/Spectre.git", majorVersion: 0, minor: 7),
-  ]
+    .package(url:"https://github.com/kylef/Spectre.git", .upToNextMinor(from:"0.8.0"))
+  ],
+  targets: [
+    .target(name: "PathKit", dependencies: [], path: "Sources"),
+    .testTarget(name: "PathKitTests", dependencies: ["PathKit", "Spectre"], path:"Tests/PathKitTests")
+  ],
+  swiftLanguageVersions: [3]
 )
