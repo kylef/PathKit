745db726d7bf10966c952445b2a134d98302f0c2
Keith Smiley <keithbsmiley@gmail.com>
2018-09-10 11:16:23 -0700

    feat: Support building with Swift 4.2 (#56)
    This likely shouldn't be merged until Swift 4.2 is officially released
    (hopefully this week)

---
+++ Package@swift-4.2.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:4.2
+import PackageDescription
+
+let package = Package(
+  name: "PathKit",
+  products: [
+    .library(name: "PathKit", targets: ["PathKit"]),
+  ],
+  dependencies: [
+    .package(url:"https://github.com/kylef/Spectre.git", .upToNextMinor(from:"0.8.0"))
+  ],
+  targets: [
+    .target(name: "PathKit", dependencies: [], path: "Sources"),
+    .testTarget(name: "PathKitTests", dependencies: ["PathKit", "Spectre"], path:"Tests/PathKitTests")
+  ],
+  swiftLanguageVersions: [.v3, .v4, .v4_2]
+)
