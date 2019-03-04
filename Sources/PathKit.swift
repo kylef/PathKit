f58181327a8ec504545bb56cebb6de040484f4d2
Elliott Williams <e@elliottwillia.ms>
2019-03-03 18:08:15 -0800

    Add relativePath(from:) and remove redundant ".."s in normalize()

--- Sources/PathKit.swift
+++ Sources/PathKit.swift
@@ -144,7 +144,13 @@ extension Path {
   ///   representation.
   ///
   public func normalize() -> Path {
-    return Path(NSString(string: self.path).standardizingPath)
+    var path = Path(NSString(string: self.path).standardizingPath)
+    if !path.isAbsolute {
+      // standardizingPath only cleans up redundant ".." if the path is absolute. We can perform this normalization
+      // by recombining the path's components, since + will backtrack on parent directory references.
+      path = path.components.reduce(Path(), +)
+    }
+    return path
   }
 
   /// De-normalizes the path, by replacing the current user home directory with "~".
@@ -250,6 +256,60 @@ extension Path {
 
     return pathExtension
   }
+
+  /// Returns the relative path necessary to go from `base` to `self`.
+  ///
+  /// Both paths must be absolute or relative paths.
+  /// - throws: Throws an error when the path types do not match, or when `base` has so many parent path components
+  ///           that it refers to an unknown parent directory.
+  public func relativePath(from base: Path) throws -> Path {
+    enum PathArgumentError: Error {
+      /// Can't back out of an unknown parent directory
+      case unknownParentDirectory
+      /// It's impossible to determine the path between an absolute and a relative path
+      case unmatchedAbsolutePath
+    }
+
+    func pathComponents(for path: ArraySlice<String>, relativeTo base: ArraySlice<String>, memo: [String]) throws -> [String] {
+      switch (base.first, path.first) {
+      // Base case: Paths are equivalent
+      case (.none, .none):
+        return memo
+
+      // No path to backtrack from
+      case (.none, .some(let rhs)):
+        guard rhs != "." else {
+          // Skip . instead of appending it
+          return try pathComponents(for: path.dropFirst(), relativeTo: base, memo: memo)
+        }
+        return try pathComponents(for: path.dropFirst(), relativeTo: base, memo: memo + [rhs])
+
+      // Both sides have a common parent
+      case (.some(let lhs), .some(let rhs)) where lhs == rhs:
+        return try pathComponents(for: path.dropFirst(), relativeTo: base.dropFirst(), memo: memo)
+
+      // `base` has a path to back out of
+      case (.some(let lhs), _):
+        guard lhs != ".." else {
+          throw PathArgumentError.unknownParentDirectory
+        }
+        guard lhs != "." else {
+          // Skip . instead of resolving it to ..
+          return try pathComponents(for: path, relativeTo: base.dropFirst(), memo: memo)
+        }
+        return try pathComponents(for: path, relativeTo: base.dropFirst(), memo: memo + [".."])
+      }
+    }
+
+    guard isAbsolute && base.isAbsolute || !isAbsolute && !base.isAbsolute else {
+      throw PathArgumentError.unmatchedAbsolutePath
+    }
+
+    return Path(components: try pathComponents(for: ArraySlice(normalize().components),
+                                               relativeTo: ArraySlice(base.normalize().components),
+                                               memo: []))
+  }
+
 }
 
 
